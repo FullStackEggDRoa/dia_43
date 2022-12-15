745eb3f9db0c60fcae85592491a7d81e94166c83
Daniel Roa <droa@apgslt.com>
2022-12-14 22:03:29 -0500

    Ejercicio_8 Avance

--- Ejercicio_8/src/ejercicio_8/Ejercicio_8.java
+++ Ejercicio_8/src/ejercicio_8/Ejercicio_8.java
@@ -15,6 +15,8 @@ public class Ejercicio_8 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
     }
     
 }
---
+++ Ejercicio_8/src/ejercicio_8/Entidades/Cadena.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio_8.Entidades;
+
+/**
+ *
+ * @author droa
+ */
+public class Cadena {
+    // Atributos
+    
+    private String frase;
+    private int longitud;
+    
+    // Contrusctores
+
+    public Cadena(String frase) {
+        this.frase = frase;
+    }
+    
+    
+    
+}
